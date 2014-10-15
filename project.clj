82b8741b0790064d23c4d770eb63515485beed11
Pedro Santos <donbonifacio@gmail.com>
2014-10-15 22:14:21 +0100

    Add servlet's dependecy

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.8.0"]
                  [org.clojure/data.json "0.2.5"]
+                 [javax.servlet/servlet-api "2.5"]
                  [compojure "1.2.0"]]
 
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler obb-rules-api.routes/app}
-  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
