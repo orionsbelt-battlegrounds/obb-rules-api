a619a867b0995532329a50fad8160be579059030
Pedro Santos <donbonifacio@gmail.com>
2014-10-15 22:26:28 +0100

    Update for heroku

--- project.clj
+++ project.clj
@@ -10,9 +10,14 @@
                  [clj-time "0.8.0"]
                  [org.clojure/data.json "0.2.5"]
                  [javax.servlet/servlet-api "2.5"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [environ "0.5.0"]
                  [compojure "1.2.0"]]
 
   :plugins [[lein-ring "0.8.12"]]
   :main obb-rules-api.routes/app
   :ring {:handler obb-rules-api.routes/app}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "obb-api-rules-standalone.jar"
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}
+             :production {:env {:production true}}})
