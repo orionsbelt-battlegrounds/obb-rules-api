698d34ad8da468635c1faccbdb69aa53904160ac
Pedro Santos <donbonifacio@gmail.com>
2014-10-15 22:28:16 +0100

    Heroku configs

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
                  [environ "0.5.0"]
                  [compojure "1.2.0"]]
 
-  :plugins [[lein-ring "0.8.12"]]
+  :plugins [[environ/environ.lein "0.2.1"]
+            [lein-ring "0.8.12"]]
   :main obb-rules-api.routes/app
   :ring {:handler obb-rules-api.routes/app}
   :hooks [environ.leiningen.hooks]
