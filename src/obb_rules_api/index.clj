8b519fefab5e105386831deac8008d7021576021
Pedro Santos <donbonifacio@gmail.com>
2014-10-16 20:01:17 +0100

    Add more info to index

--- src/obb_rules_api/index.clj
+++ src/obb_rules_api/index.clj
@@ -1,8 +1,14 @@
 (ns obb-rules-api.index
-  (:require [obb-rules-api.reply :as reply]))
+  (:require [obb-rules-api.reply :as reply]
+            [obb-rules.turn :as turn]
+            [obb-rules.game :as game]
+            [obb-rules.actions.move :as move]))
 
 (defn handler
   "Processes the root endpoint"
   []
   (reply/ok {:name "obb-rules-api"
-             :version (System/getProperty "obb-rules-api.version")}))
+             :version (System/getProperty "obb-rules-api.version")
+             :rules {:version game/version
+                     :max-action-points turn/max-action-points
+                     :min-move-percentage move/min-move-percentage}}))
