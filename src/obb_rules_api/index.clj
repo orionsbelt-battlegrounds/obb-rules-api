594c175cfa363b39766c2fd556e73adc25ff9e43
Pedro Santos <donbonifacio@gmail.com>
2014-10-19 15:31:20 +0100

    Add default board size

--- src/obb_rules_api/index.clj
+++ src/obb_rules_api/index.clj
@@ -2,6 +2,7 @@
   (:require [obb-rules-api.reply :as reply]
             [obb-rules.turn :as turn]
             [obb-rules.game :as game]
+            [obb-rules.board :as board]
             [obb-rules.actions.move :as move]))
 
 (defn handler
@@ -10,5 +11,7 @@
   (reply/ok {:name "obb-rules-api"
              :version (System/getProperty "obb-rules-api.version")
              :rules {:version game/version
+                     :default-board-width board/default-board-w
+                     :default-board-height board/default-board-h
                      :max-action-points turn/max-action-points
                      :min-move-percentage move/min-move-percentage}}))
