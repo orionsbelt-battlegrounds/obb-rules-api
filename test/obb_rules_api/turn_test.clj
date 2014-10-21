2182a0cb182b1df662b25f1398bdceeb66690ab4
Pedro Santos <donbonifacio@gmail.com>
2014-10-21 15:01:18 +0100

    Start p2 focus test

--- test/obb_rules_api/turn_test.clj
+++ test/obb_rules_api/turn_test.clj
@@ -24,3 +24,24 @@
                                 [:attack [1 3] [1 4]]]})]
     (is (not= :deploy (get-in result [:board :state])))))
 
+(deftest test-focus-p2-turn
+  (let [board {:game {:state :p2
+                      :elements {[1 1] {:player :p1
+                                        :unit (unit/fetch :kamikaze)
+                                        :quantity 1
+                                        :coordinate [1 1]
+                                        :direction :south}
+                                 [4 4] {:player :p2
+                                        :unit (unit/fetch :rain)
+                                        :quantity 1
+                                        :coordinate [4 4]
+                                        :direction :east}}}}
+        direct-actions [[:move [4 4] [3 3] 1]
+                        [:move [3 3] [2 2] 1]
+                        [:move [2 2] [2 1] 1]
+                        [:rotate [2 1] :west]
+                        [:attack [2 1] [1 1]]]]
+    (testing "regular :p2"
+      (let [direct-board (assoc board :actions direct-actions)
+            [direct result response] (sim/turn-ok :p2 direct-board)]
+      ))))
