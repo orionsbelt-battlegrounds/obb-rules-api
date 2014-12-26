6723666d3a436697022bc3b105a83cb54929089e
Pedro Santos <donbonifacio@gmail.com>
2014-12-26 15:03:12 +0000

    obb-rules 1.6

--- src/obb_rules_api/index.clj
+++ src/obb_rules_api/index.clj
@@ -1,9 +1,7 @@
 (ns obb-rules-api.index
   (:require [obb-rules-api.reply :as reply]
-            [obb-rules.turn :as turn]
             [obb-rules.game :as game]
-            [obb-rules.board :as board]
-            [obb-rules.actions.move :as move]))
+            [obb-rules.laws :as laws]))
 
 (defn handler
   "Processes the root endpoint"
@@ -11,7 +9,7 @@
   (reply/ok {:name "obb-rules-api"
              :version (System/getProperty "obb-rules-api.version")
              :rules {:version game/version
-                     :default-board-width board/default-board-w
-                     :default-board-height board/default-board-h
-                     :max-action-points turn/max-action-points
-                     :min-move-percentage move/min-move-percentage}}))
+                     :default-board-width laws/default-board-w
+                     :default-board-height laws/default-board-h
+                     :max-action-points laws/max-action-points
+                     :min-move-percentage laws/min-move-percentage}}))
